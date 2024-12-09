1fd5f34053a0c267ac1653f43ab1e9db9eccf54f
Shayan Janjua <shayanjanjua.london@gmail.com>
2024-12-09 00:16:13 +0000

    Started work on ui, need to strart mysql

--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
 'use client'
-
+import { useEffect } from "react";
+import { useState } from "react";
+import Button from '@mui/material/Button';
+import { Container, Stack, SvgIcon, Typography } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { CenterFocusStrong, Rowing } from "@mui/icons-material";
+import NotificationsNoneSharpIcon from '@mui/icons-material/NotificationsNoneSharp';
+import NotificationsSharpIcon from '@mui/icons-material/NotificationsSharp';
+import MessageSharpIcon from '@mui/icons-material/MessageSharp';
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 
 export default function Home() {
   let ws;
@@ -7,15 +19,16 @@ export default function Home() {
     ws.onerror = ws.onopen = ws.onclose = ws.onmessage = null;
   }
 
-  ws = new WebSocket("ws://localhost:8080");
+  ws = new WebSocket("https://websocket-database-pinging-production.up.railway.app");
 
   ws.onopen = () => {
     console.log("Connected");
-    ws.send("Hello,what");
+    ws.send("Hello,start,connect");
   }
 
   ws.onmessage = (msg) =>{
     console.log(msg.data);
+    ws.send("Hello, John")
   }
 
   ws.onclose = () => {
@@ -26,8 +39,18 @@ export default function Home() {
     ws = null;
   }
   return (
-    <div>
-      <label>Hello World!</label>
-    </div>
+    <AppBar position="static" color="white">
+      <Stack direction="row" justifyContent={"space-between"} alignItems={"center"} >
+      <Typography >CSS</Typography>
+      <Stack direction="row">
+        <Button variant="text"><HomeRoundedIcon/></Button>
+        <Button variant="text"><SearchSharpIcon/></Button>
+        <Button variant="text"><MessageSharpIcon/></Button>
+        <Button variant="text"><NotificationsNoneSharpIcon/></Button>
+        <Button variant="text"><AccountCircleIcon/></Button>
+      </Stack>
+      <Button><SettingsRoundedIcon/></Button>
+      </Stack>
+    </AppBar>
   );
 }
