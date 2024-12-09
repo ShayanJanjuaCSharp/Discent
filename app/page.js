cf170829019b05672328ee2c0b4d8a7013a60f0e
Shayan Janjua <shayan.janjua@city.ac.uk>
2024-12-09 11:00:54 +0000

    ui changes

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Button from '@mui/material/Button';
-import { Container, Stack, SvgIcon, Typography } from "@mui/material";
+import { Box, Container, Divider, Stack, SvgIcon, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { CenterFocusStrong, Rowing } from "@mui/icons-material";
@@ -12,7 +12,7 @@ import MessageSharpIcon from '@mui/icons-material/MessageSharp';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-
+import TextField from '@mui/material/TextField';
 export default function Home() {
   let ws;
   if(ws){
@@ -37,9 +37,10 @@ export default function Home() {
 
   ws.onclose = () => {
     ws = null;
-  }
+  }   
   return (
-    <AppBar position="static" color="white">
+    <Box sx={{width:"100vw", height:"100vh"}} justifyContent={"space-around"}>
+    <AppBar position="static"  color="white" sx={{width:"80vw",left:"10vw"}} px={4} >
       <Stack direction="row" justifyContent={"space-between"} alignItems={"center"} >
       <Typography >CSS</Typography>
       <Stack direction="row">
@@ -52,5 +53,14 @@ export default function Home() {
       <Button><SettingsRoundedIcon/></Button>
       </Stack>
     </AppBar>
+    <Stack direction="column" justifyItems={"space-around"} alignItems={"center"} spacing={2} sx={{ marginTop: "20px" }}>
+      <TextField placeholder="Search" sx={{width:"700px", height:"100px"}}></TextField>
+      <Divider orientation="horizontal" flexItem></Divider>
+      <Stack direction="column" >
+
+      </Stack>
+    </Stack>
+    </Box>
+    
   );
 }
